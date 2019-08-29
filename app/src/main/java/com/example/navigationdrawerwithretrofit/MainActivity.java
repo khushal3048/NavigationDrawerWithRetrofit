e833bf94fc953682abdcabe9bbd1947fffa251f0
Khushal Kakadiya <khushalkakadiya3048@gmail.com>
2019-08-29 15:07:10 -0400

    Navigation Drawer Complete

--- app/src/main/java/com/example/navigationdrawerwithretrofit/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawerwithretrofit/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigation();
     }
 
     @Override
