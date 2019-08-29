e051b629ae9dea31995f318517384c23c3fbd3b9
Khushal Kakadiya <khushalkakadiya3048@gmail.com>
2019-08-29 14:09:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrawerwithretrofit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.navigationdrawerwithretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
