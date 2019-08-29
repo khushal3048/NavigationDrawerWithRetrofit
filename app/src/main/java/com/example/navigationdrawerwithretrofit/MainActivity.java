e147400f19df20c68951c740b6c15488ece67f46
Khushal Kakadiya <khushalkakadiya3048@gmail.com>
2019-08-29 14:47:05 -0400

    Navigation Drawer Connected with Host Fragment

--- app/src/main/java/com/example/navigationdrawerwithretrofit/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawerwithretrofit/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.navigationdrawerwithretrofit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public Toolbar toolbar;
+    public DrawerLayout drawerLayout;
+    public NavigationView navigationView;
+    public NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.host_fragment),drawerLayout);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+
+    public void setupNavigation(){
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.navigationview);
+
+        navController = Navigation.findNavController(this,R.id.host_fragment);
+
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        menuItem.setCheckable(true);
+
+        drawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+
+        switch (id){
+
+            case R.id.home:
+                navController.navigate(R.id.defaultFragment);
+                break;
+            case R.id.pokemonlist:
+                navController.navigate(R.id.recyclerviewFragment);
+                break;
+        }
+
+        return true;
     }
 }
